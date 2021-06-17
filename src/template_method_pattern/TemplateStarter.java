c4bc890a442c03ce35aff51504ea1bcff1dd27a3
Lovász Ákos <akos4x44@gmail.com>
2021-06-17 16:18:50 +0200

    removed todo comment

--- src/template_method_pattern/TemplateStarter.java
+++ src/template_method_pattern/TemplateStarter.java
@@ -5,7 +5,6 @@ import template_method_pattern.implementations.User;
 public class TemplateStarter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		User u = new User("xd");
 		u.Save();
 		
